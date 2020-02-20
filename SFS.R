d5f990834baaf5500d279e3fd2863d3e28c9c871
anoobvinu07 <42816154+anoobvinu07@users.noreply.github.com>
2020-02-20 10:45:00 -0500

    fixed the toc issue

---
+++ SFS.R
@@ -0,0 +1,31 @@
+
+setwd("C:\\Users\\Anoob\\Desktop\\SFS_EcoGen")
+
+list.files()
+
+SFS <- scan("XGL_outFold.sfs")
+
+sumSFS <- sum(SFS)
+
+pctPoly <- 100*(1-(SFS[1]/sumSFS))
+
+plotSFS <- SFS[-c(1,length(SFS))]
+
+barplot(plotSFS)
+
+div <- read.table("XGL_folded_allsites.thetas.idx.pestPG")
+
+colnames(div) <- c("window", "chrname","wincenter","tW","tP","tF","tH","tL",
+                   "tajD","fulif","fuliD","fayH","zengsE","numSites")
+
+div$tWpersite <- div$tW/div$numSites
+div$tPpersite <- div$tP/div$numSites
+
+par(mfrow=c(2,2))
+hist(div$tWpersite, col="gray",xlab="Theta-W",main="")
+hist(div$tPpersite, col="gray",xlab="Theta-P",main="")
+hist(div$tajD, col="gray",xlab="Tajima's D",main="")
+barplot(plotSFS)
+dev.off() # closes the pdf and writes it to file
+
+summary(div)
