0d2ebab8e1ea86136a77e48ff8474a4fe5067c60
anoobvinu07 <42816154+anoobvinu07@users.noreply.github.com>
2020-04-20 22:46:33 -0400

    GWAS - estimating blups and filtering bam files

--- myscripts/bedtools.sh
+++ myscripts/bedtools.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/users/a/p/ap1/EcologicalGenomics/myresults/bedfiles_copepod/diffmeth.bed closest -a diffmeth.bed \
+/data/popgen/bedtools2/bin/bedtools closest -a /users/a/p/ap1/EcologicalGenomics/myresults/bedfiles_copepod/diffmeth.bed\
       -b /data/project_data/epigenetics/GCA_900241095.1_Aton1.0_genomic.fa_annotation_table.bed \
       -D b | \
-      awk '!($10=="-")' > hits.bed 
+      awk '!($10=="-")' > hits.bed # save filename is hits.bed
